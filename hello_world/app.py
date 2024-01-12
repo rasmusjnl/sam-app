0f0190166b94aa144ae7279066f187cdb103454b
EC2 Default User <ec2-user@ip-172-31-1-246.eu-west-1.compute.internal>
2024-01-12 08:30:41 +0000

    Enabled tests. Updated lambda

--- hello_world/app.py
+++ hello_world/app.py
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello skrt",
+            "message": "hello UPDATED!",
             # "location": ip.text.replace("\n", "")
         }),
     }
